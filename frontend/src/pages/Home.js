b913687e809b7e3aa05c4927f63b2f54a602b92d
sarahchoww <chow.sarah@hotmail.com>
2023-11-20 16:07:44 -0500

    for progress report - looking prettier lol

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,25 @@
 import '../assets/Home.css';
 import armPic from '../assets/images/arm.png'
-import ellipse from '../assets/images/ellipse-home.png'
 
 
 const Home = () => {
+
     return(
         <div className="bg-image">
 
-            {/* <img src={armPic} className="App-logo" alt="arm" /> */}
+            <div className="App-group">
+                <a className="App-link" href="/">
+                    Home
+                    </a>
+                    <a className="App-link" href="/contact">
+                    Contact
+                    </a>
+                    <a className="App-link" href="/login">
+                    Log In
+                    </a>
+            </div>
+
+            <img src={armPic} className="App-logo" alt="arm" />
             
         </div>
     )
