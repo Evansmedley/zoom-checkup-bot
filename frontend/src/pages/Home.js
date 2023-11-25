246f36b5301e097a90fed13208c69e88ac5e527b
rheophile10 <icar@rheophile.ca>
2023-11-24 22:20:23 -0600

    :D

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,14 @@
 import '../assets/Home.css';
 import armPic from '../assets/images/arm.png'
 import ellipse from '../assets/images/ellipse-home.png'
-
+import Fun from './Fun'
 
 const Home = () => {
     return(
         <div className="bg-image">
 
             {/* <img src={armPic} className="App-logo" alt="arm" /> */}
-            
+            <Fun />
         </div>
     )
 };
