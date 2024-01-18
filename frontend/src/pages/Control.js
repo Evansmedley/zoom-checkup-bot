4117cafc5cc21cadd4f0aeb6865d9624114e5620
Elisha Catherasoo <elisha123jc@gmail.com>
2024-01-17 21:57:13 -0500

    Added notepad

--- frontend/src/pages/Control.js
+++ frontend/src/pages/Control.js
@@ -1,11 +1,25 @@
+import '../assets/Control.css';
+import Header from './Header';
 const Control = () => {
 
     return(
 
-        <div>
-            control page
+        <div class="main-control">
+            <Header />
+            <div class="column">*Add livestream here*</div>
+            <div class="column">
+                Notes <br />
+                <textarea class="notes" rows="15" cols="70" placeholder="Write notes here..." download="final_notes"></textarea>
+                <br /><br />
+                <button type='button' class="saveBtn" onclick="download('notes.value', 'filename.value'"> Download</button>
+                <input class="filename" placeholder="Specify a filename…" />
+            </div>
         </div>
     )
 };
+
+function download(text, filename) {
+    console.log(5 + 6);
+}
 
 export default Control;
