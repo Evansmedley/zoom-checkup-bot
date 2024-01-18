01ef92e19a58d57dca5060f8239e8672d52e41fb
sarahchow <chow.sarah@hotmail.com>
2024-01-18 18:03:09 -0500

    landing page resize

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,20 @@
-
 const Home = () => {
-
-    return(
-        <div className="bg-sphere">
-
-
-            <div className="page-content">
-                <div className="text">
-                    <div id="mainText">
-                        Your Remote Doctor is here to assist
-                    </div>
-
-
-                    <div id="subText">
-                        Get your hands-on doctors appointment from the convenience of your home.
-                    </div>
-                </div>
-
-
-                <img src={'/assets/images/arm.png'} className="robot-pic" alt="arm" />
-            
-            </div>
+  return (
+    <div className="page-content">
+      <div className="text">
+        <div id="mainText">Your Remote Doctor is here to assist</div>
+        <div id="subText">
+          Get your hands-on doctors appointment from the convenience of your
+          home.
         </div>
-    )
+      </div>
+      <img src={"/assets/images/arm.png"} className="robot-pic" alt="arm" />
+
+      <div className="bg-sphere">
+        <div className="bg-square"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
