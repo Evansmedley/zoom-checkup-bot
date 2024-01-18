58839b85bafb12a6b672c8ed79539c6f4488aaf6
Elisha Catherasoo <elisha123jc@gmail.com>
2024-01-18 10:52:39 -0500

    Added project files

---
+++ checkup-bot-backend/src/test/java/com/checkupbot/checkupbotbackend/CheckupBotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkupbot.checkupbotbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckupBotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
