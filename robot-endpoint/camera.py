d5224eec4ef6ec58d34e50261a82e7ee1c40e23f
deestroy <dhritiaravind@gmail.com>
2024-01-21 21:50:59 -0500

    changed the motor control to correspond to buttons on UI

--- robot-endpoint/camera.py
+++ robot-endpoint/camera.py
@@ -1,2 +1,38 @@
-import cv2
+import cv2 as cv
 import threading
+from time import sleep
+import ipywidgets as widgets
+from IPython.display import display
+
+cam = cv.VideoCapture(0)
+
+class RobotAuto:
+
+    def find_mouth(self, img):
+        """Finds the mouth and draws a bounding box
+        """
+        pass
+    
+    def find_funny_bone(self, img):
+        """Finds the ulnar name and draws bounding box
+        """
+        pass
+
+    def flashlight(self, button: bool):
+        """Turns on and off the flashlight
+        """
+        pass
+
+def camera_start():
+    cam = cv.VideoCapture(0)
+    while cam.isOpened():
+        try:
+            ret, img = cam.read()
+            img = cv.resize(img, (640, 480))
+        except KeyboardInterrupt:cam.release()
+
+
+camera_start()
+
+
+
--- robot-endpoint/main.py
+++ robot-endpoint/main.py
@@ -1,22 +1,18 @@
 import time
 import threading
 from Arm_Lib import Arm_Device
-# import cv2
-import numpy as np
 from dataclasses import dataclass
 
 MOVEMENT_TIME = 100
-STEP_ANGLE = 1
-
-Arm = Arm_Device()
-time.sleep(.1)
+STEP_ANGLE = 15
+STEP_ANGLE_MOV = 10
 
 @dataclass
 class MotorAngle:
     motor_id: int
     min_angle: int
     max_angle: int
-    curr_angle: float
+    curr_angle: int
     time_run: int
 
 angle1 = MotorAngle(1, 0, 180, 0, MOVEMENT_TIME)
@@ -26,30 +22,21 @@ angle4 = MotorAngle(4, 0, 180, 90, MOVEMENT_TIME)
 angle5 = MotorAngle(5, 0, 180, 90, MOVEMENT_TIME)
 angle6 = MotorAngle(6, 0, 165, 90, 500)
 
-def reset_robot():
-    '''
-    Robot init
-    '''
-    # Alert patient with buzzer
-    b_time = 1
-    Arm.Arm_Buzzer_On(b_time)
-    time.sleep(1)
-    # Closed buzzer
-    Arm.Arm_Buzzer_Off()
-    time.sleep(1)
-
-    # Set to default position
-    Arm.Arm_serial_servo_write6(0, 90, 90, 90, 90, 90, 500)
-    time.sleep(1)
-    return
-
-def move_bot(key_press: str) -> None:
-    """Moves robot based on key press
-
-    Args:
-        key_press (str): user input key
+class BotActions:
+    """Bot Action Class
     """
-    def boundaries(next_angle: float, angle_num: MotorAngle) -> MotorAngle:
+    def __init__(self, Arm, angle1, angle2, angle3, angle4, angle5, angle6) -> None:
+        self.Arm = Arm
+        self.angle1 = angle1
+        self.angle2 = angle2
+        self.angle3 = angle3
+        self.angle4 = angle4
+        self.angle5 = angle5
+        self.angle6 = angle6
+
+        self.reset_motors()
+
+    def boundaries(self, next_angle: float, angle_num: MotorAngle) -> MotorAngle:
         """ Forces the robot to stay within the boundaries
 
         Args:
@@ -65,53 +52,108 @@ def move_bot(key_press: str) -> None:
             angle_num.curr_angle = angle_num.min_angle
         else:
             angle_num.curr_angle = next_angle
-        return angle_num
+        return angle_num.curr_angle
     
-    # left and right controlled by Motor 1 (keys A and D)
-    # large range up and down controlled by Motor 2 (keys W and S)
-    # medium range up and down controlled by Motor 3 (keys R and F)
-    # small range up and down controlled by Motor 4 (keys I and K)
-    # swivel of claw controlled by Motor 5 (keys left and right)
-    # opening of claw controlled by Motor 6 (keys up and down)
-    # space bar to reset to default position
+    def reset_motors(self) -> None:
+        """ 
+        Resets all motor angles after a delay of 3 seconds
+        """
+        time.sleep(3)
+        buzzer_time = 1 # Alert with buzzer
+        self.Arm.Arm_Buzzer_On(buzzer_time)
+        time.sleep(1)
 
-    if key_press == "space_bar":
-        time.sleep(5)
-        reset_robot()
+        # Closed buzzer
+        self.Arm.Arm_Buzzer_Off()
+        time.sleep(1)
 
-    key_dict = {
-        'A': (angle1, STEP_ANGLE),
-        'D': (angle1, 0-STEP_ANGLE),
-        'W': (angle2, STEP_ANGLE),
-        'S': (angle2, 0-STEP_ANGLE),
-        'R': (angle3, STEP_ANGLE),
-        'F': (angle3, 0-STEP_ANGLE),
-        'I': (angle4, STEP_ANGLE),
-        'K': (angle4, 0-STEP_ANGLE),
-        'left': (angle5, STEP_ANGLE),
-        'right': (angle5, 0-STEP_ANGLE),
-        'up': (angle6, STEP_ANGLE),
-        'down': (angle6, 0-STEP_ANGLE),
-    }
+        # Set to default position
+        self.Arm.Arm_serial_servo_write6(0, 90, 90, 90, 90, 90, 500)
+        time.sleep(1)
 
-    key_value = key_dict.get(key_press)
-    motor_angle, new_step_angle = key_value
+        self.update_position()
 
-    motor_angle = boundaries(motor_angle.curr_angle + new_step_angle, motor_angle)
-    Arm.Arm_serial_servo_write(motor_angle.motor_id, motor_angle.curr_angle, motor_angle.time_run)
-    time.sleep(0.01)
+    def update_position(self) -> None:
+        """
+        Updates the location of all motor angles
+        """
+        self.angle1.curr_angle = self.Arm.Arm_serial_servo_read(self.angle1.motor_id)
+        self.angle2.curr_angle = self.Arm.Arm_serial_servo_read(self.angle2.motor_id)
+        self.angle3.curr_angle = self.Arm.Arm_serial_servo_read(self.angle3.motor_id)
+        self.angle4.curr_angle = self.Arm.Arm_serial_servo_read(self.angle4.motor_id)
+        self.angle5.curr_angle = self.Arm.Arm_serial_servo_read(self.angle5.motor_id)
+        self.angle6.curr_angle = self.Arm.Arm_serial_servo_read(self.angle6.motor_id)
 
-def grab_tool():
-    pass
+    def set_motor(self, motor: MotorAngle, ang: int) -> None:
+        """
+        Sets the position of the angle for each motor
+
+        Args:
+            motor (MotorAngle): motor to set
+            ang (int): angle to set
+        """
+        self.Arm.Arm_serial_servo_write(motor.motor_id, ang, motor.time_run)
+        time.sleep(0.5)
+        self.update_position()
+
+    def open_claw(self) -> None:
+        """
+        Opens end effector
+        """
+        self.set_motor(self.angle6.motor_id, self.angle6.max_angle)
+    
+    def close_claw(self) -> None:
+        """
+        Closes end effector
+        """
+        motor_angle = self.boundaries(self.angle6.curr_angle + STEP_ANGLE, self.angle6)
+        self.set_motor(self.angle6, motor_angle)
+    
+    def turn_right(self) -> None:
+        """
+        Turns robot right
+        """
+        motor_angle = self.boundaries(self.angle1.curr_angle - STEP_ANGLE_MOV, self.angle1)
+        self.set_motor(self.angle1, motor_angle)
+    
+    def turn_left(self) -> None:
+        """
+        Turns robot left
+        """
+        motor_angle = self.boundaries(self.angle1.curr_angle + STEP_ANGLE_MOV, self.angle1)
+        self.set_motor(self.angle1, motor_angle)
+
+    def move_forwards(self) -> None:
+        """
+        Moves robot forward
+        """
+        # TODO: change move forwards so end effector remains parallel with the table when moving
+        self.angle2.curr_angle = 90
+        self.angle3.curr_angle = 0
+        self.angle4.curr_angle = 90
+
+        self.set_motor(self.angle2, self.angle2.curr_angle)
+        self.set_motor(self.angle3, self.angle3.curr_angle)
+        self.set_motor(self.angle4, self.angle4.curr_angle)
+
+    def move_backwards(self) -> None:
+        """
+        Moves Robot backwards
+        """
+        # TODO: change move backwards so end effector remains parallel with the table when moving
+        self.set_motor(self.angle2, self.angle2.curr_angle)
+        self.set_motor(self.angle3, self.angle3.curr_angle)
+        self.set_motor(self.angle4, self.angle4.curr_angle)
 
 
 if __name__ == "__main__":
-    reset_robot()
+    Arm = Arm_Device()
+    time.sleep(.1)
+    move_bot = BotActions(Arm, angle1, angle2, angle3, angle4, angle5, angle6)
+
     th1 = threading.Thread(target=move_bot)
     th1.setDaemon(True)
     th1.start()
-
-    # need to add a error handler
 
     # Stops arm
     del Arm
