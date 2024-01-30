30bbe5050fde11095ecb63f3692a5dbbbd3c3cce
deestroy <dhritiaravind@gmail.com>
2024-01-29 19:08:07 -0500

    Updated camera in line with test results

--- robot-endpoint/camera.py
+++ robot-endpoint/camera.py
@@ -1,61 +1,24 @@
-import numpy as np
-import cv2 as cv
+import cv2
+import ipywidgets.widgets as widgets
+import threading
+import time
 
+import enum
+import cv2
+    
+image = cv2.VideoCapture(0, cv2.CAP_GSTREAMER)
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+width=600
+height=500
+try:
+    while True:
+        ret, frame = image.read()
+        cv2.imshow("Camera Stream", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break # wait until a key is pressed
+except KeyboardInterrupt:
+    print(" Program closed!")
+    pass
 
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
-
-
-# import cv2
-# import ipywidgets.widgets as widgets
-# import threading
-# import time
-# import enum
-
-# image_widget = widgets.Image(format='jpeg', width=600, height=500)
-# display(image_widget) # Display camera widget
-
-# def bgr8_to_jpeg(value, quality=75):
-#     """
-#     bgr8 to jpeg converter
-#     """
-#     return bytes(cv2.imencode('.jpg', value)[1])
-
-# image = cv2.VideoCapture(0)                           
-
-# width=600
-# height=500
-# ret, frame = image.read()    
-# image_widget.value = bgr8_to_jpeg(frame)
-
-# try:
-#     while 1:
-#         ret, frame = image.read()
-#         image_widget.value = bgr8_to_jpeg(frame)
-#         time.sleep(0.010)
-# except KeyboardInterrupt:
-#     print(" Program closed! ")
-#     pass
-
-# image.release() 
+image.release()
+cv2.destroyAllWindows()
