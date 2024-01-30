a980d77c25959a3ff86de23e19a9c1fc9dfbf1f0
deestroy <dhritiaravind@gmail.com>
2024-01-29 19:29:14 -0500

    Sucessful detection of a face with test results

--- robot-endpoint/camera.py
+++ robot-endpoint/camera.py
@@ -5,7 +5,35 @@ import time
 
 import enum
 import cv2
+
+def face_filter(faces):
+
+    if len(faces) == 0: return None
+
+    max_face = max(faces, key=lambda face: face[2] * face[3])
+    (x, y, w, h) = max_face
+
+    if w < 10 or h < 10: return None
+    return max_face
     
+
+def follow_function(img):
+    img = cv2.resize(img, (640, 480))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img.copy()
+    faces = faceDetect.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+    if len(faces) != 0:
+        face = face_filter(faces)
+
+        (x, y, w, h) = face
+
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+        cv2.putText(img, 'Person', (280, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (105, 105, 105), 2)
+        point_x = x + w / 2
+        point_y = y + h / 2
+    return img
+
+faceDetect = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 image = cv2.VideoCapture(0, cv2.CAP_GSTREAMER)
 
 width=600
@@ -13,7 +41,8 @@ height=500
 try:
     while True:
         ret, frame = image.read()
-        cv2.imshow("Camera Stream", frame)
+        img = follow_function(frame)
+        cv2.imshow("Camera Stream", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break # wait until a key is pressed
 except KeyboardInterrupt:
