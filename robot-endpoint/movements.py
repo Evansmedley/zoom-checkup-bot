d44c9f21d1c634a416bec79da18d80ce94086704
deestroy <dhritiaravind@gmail.com>
2024-01-31 18:00:08 -0500

    print read

--- robot-endpoint/movements.py
+++ robot-endpoint/movements.py
@@ -77,6 +77,7 @@ class Move_Motors:
         list_angles = [self.motor1.curr_angle, self.motor2.curr_angle, self.motor3.curr_angle, self.motor4.curr_angle, self.motor5.curr_angle,  self.motor6.curr_angle]
         list_angles = [90, 10, 0, 0, 0, 0]
         fwd_k = ForwardKinematics(list_angles, [0, 0, 0, 0, 0, 0])
+        print("FWD_K:", fwd_k)
 
     
 
