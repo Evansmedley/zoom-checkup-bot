7ae534e6e945ecfe01e771955643936cc47f0e6f
Jeremy Trendoff <jeremytrendoff@rogers.com>
2024-01-31 19:26:57 -0500

    JWT's are valid for 24 hours

--- checkup-bot-backend/src/main/java/com/checkupbot/checkupbotbackend/services/JwtService.java
+++ checkup-bot-backend/src/main/java/com/checkupbot/checkupbotbackend/services/JwtService.java
@@ -36,7 +36,7 @@ public class JwtService {
         return Jwts.builder().setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + (1000 * 60 * 24)))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS512)
                 .compact();
     }
